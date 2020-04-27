580a3b5560644b1c3d4af3e828c157b87df92e12
dayveed25 <day_veed@ymail.com>
2020-04-28 00:37:43 +0100

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Mobile Nav Function
+function openNav() {
+  document.getElementById("mobile-nav").style.width = "50%";
+}
+  
+function closeNav() {
+  document.getElementById("mobile-nav").style.width = "0";
+}
